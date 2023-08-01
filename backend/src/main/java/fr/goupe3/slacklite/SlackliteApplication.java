d71c3a0953698274c71a489690aff93ea7e83473
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-08-01 16:42:30 +0200

    Feature: init spring project

---
+++ backend/src/main/java/fr/goupe3/slacklite/SlackliteApplication.java
@@ -0,0 +1,13 @@
+package fr.goupe3.slacklite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackliteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/goupe3/slacklite/SlackliteApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.goupe3.slacklite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
