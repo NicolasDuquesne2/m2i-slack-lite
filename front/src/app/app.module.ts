2c1e1eeae0e7ec3c61e0bdc0bf2ae72de33f5c4f
Loic2dot0 <moreaux.loic@gmail.com>
2023-08-28 19:27:17 +0200

    create header and footer comp, add assets

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
