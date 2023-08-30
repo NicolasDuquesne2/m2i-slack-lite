f23e8cd4176fdf9a782e375aa6103b5da43614da
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-08-30 20:25:28 +0200

    Add starter http services & config file

---
+++ front/src/app/service/http-user.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { BASE_URL } from '../config/config';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpUserService {
+  constructor() {}
+}
