f0a13c589569e21d0d1720eef4d2816915a5503c
Loic2dot0 <moreaux.loic@gmail.com>
2023-08-31 22:48:43 +0200

    update login component

--- front/src/app/component/login/login.component.ts
+++ front/src/app/component/login/login.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  formLogin: FormGroup;
+  isError: boolean = false;
+  isErrorEmail: boolean = false;
+  isErrorPassword: boolean = false;
+
+  constructor(private formBuilder: FormBuilder, ){
+    this.formLogin = this.formBuilder.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['', [Validators.required]]
+    });
+  }
+
+  onLogin(){
+    this.isError = false;
+    this.isErrorEmail = false;
+    this.isErrorPassword = false;
+    
+    if(this.formLogin.get('email')?.invalid) this.isErrorEmail = true;
+    if(this.formLogin.get('password')?.invalid) this.isErrorPassword = true;
+    if (this.formLogin.invalid) return;
+    
+    // appel API
+    this.isError = false;
+    console.log('click');
+  }
+}
