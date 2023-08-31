d25a7a2399f17964c90a35fa19779cbce9c2ced2
Loic2dot0 <moreaux.loic@gmail.com>
2023-08-31 17:42:31 +0200

    update user service, create interface user-form

---
+++ front/src/app/interface/user-form.ts
@@ -0,0 +1,7 @@
+export interface UserForm {
+    id: number | null;
+    name: string | null,
+    email: string | null,
+    password: string | null,
+    avatar: string | null
+}
--- front/src/app/service/http-user.service.ts
+++ front/src/app/service/http-user.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 import { BASE_URL } from '../config/config';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user';
+import { UserForm } from '../interface/user-form';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpUserService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(`${BASE_URL}/users`);
+  }
+
+  getUserById(id: number): Observable<User>{
+    return this.http.get<User>(`${BASE_URL}/users/${id}`);
+  }
+
+  createUser(user: UserForm){
+    const body = {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    }
+    return this.http.post(`${BASE_URL}/users/signup`, body);
+  }
+
+  loginUser(user: UserForm){
+    const body = {
+      email: user.email,
+      password: user.password,
+    }
+    return this.http.post(`${BASE_URL}/users/login`, body);
+  }
+
+  partialUpdateUser(user: UserForm){
+    const body = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      avatar: user.avatar
+    }
+    return this.http.patch(`${BASE_URL}/users/${user.id}`, body);
+  }
+
+  deleteUserById(id: number){
+    return this.http.delete(`${BASE_URL}/users/${id}`);
+  }
 }
