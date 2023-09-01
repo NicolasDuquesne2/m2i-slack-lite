94d4bed3290047b22d4a9b380298875f37db44b7
Loic2dot0 <moreaux.loic@gmail.com>
2023-09-01 19:19:59 +0200

    Update user-profile

--- front/src/app/component/user-profile/user-profile.component.ts
+++ front/src/app/component/user-profile/user-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { User } from 'src/app/interface/user';
+import { HttpUserService } from 'src/app/service/http-user.service';
+import { UserService } from 'src/app/service/user.service';
+
 
 @Component({
   selector: '[app-user-profile]',
@@ -7,5 +11,41 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class UserProfileComponent {
+  user: User | undefined;
+  id:number | null = null;
 
+  constructor(private userService: UserService, private httpUserService: HttpUserService) { }
+
+  ngOnInit(): void {
+    this.userService.userId.subscribe((observer)=>{
+      this.id = observer;
+      console.log(`userId observer: ${this.id}`);
+    });
+
+    this.userService.user.subscribe((observer)=>{
+      this.user = observer;
+      console.log(`userId observer: ${this.user}`);
+    });
+
+    if(this.id != null) {
+      this.httpUserService.getUserById(this.id).subscribe({
+        next: (res:User) => {
+          console.log(res);
+          this.userService.setUser(res);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      });
+    }
+  }
+
+  getAvatarSrc():string{
+    if(!this.user) return './assets/img/default-user.svg';
+    if( this.user.avatar == '') return './assets/img/default-user.svg';
+    if( this.user.avatar.toLowerCase() == 'avatar') return './assets/img/avatar.webp';
+    if( this.user.avatar.toLowerCase() == 'eldenring' ||  this.user.avatar.toLowerCase() == 'malenia') return './assets/img/malenia.webp';
+    if(!this.user.avatar.startsWith('http')) return './assets/img/default-user.svg';
+    return  this.user.avatar;
+  }
 }
