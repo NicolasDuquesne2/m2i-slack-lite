4c51d01e04210e9bf49b5e851080ae9b5d533cc1
Loic2dot0 <moreaux.loic@gmail.com>
2023-09-01 14:47:09 +0200

    update login form

--- front/src/app/component/login/login.component.ts
+++ front/src/app/component/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserForm } from 'src/app/interface/user-form';
+import { HttpUserService } from 'src/app/service/http-user.service';
 
 @Component({
   selector: 'app-login',
@@ -13,24 +15,24 @@ export class LoginComponent {
   isErrorEmail: boolean = false;
   isErrorPassword: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, ){
+  constructor(private formBuilder: FormBuilder, private httpUserService: HttpUserService, private router: Router) {
     this.formLogin = this.formBuilder.group({
-      email:['',[Validators.required, Validators.email]],
-      password:['', [Validators.required]]
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
     });
   }
 
-  onLogin(){
+  onLogin() {
     // Reset error and validation variables
     this.isError = false;
     this.isErrorEmail = false;
     this.isErrorPassword = false;
-    
+
     // Form validation
-    if(this.formLogin.get('email')?.invalid) this.isErrorEmail = true;
-    if(this.formLogin.get('password')?.invalid) this.isErrorPassword = true;
+    if (this.formLogin.get('email')?.invalid) this.isErrorEmail = true;
+    if (this.formLogin.get('password')?.invalid) this.isErrorPassword = true;
     if (this.formLogin.invalid) return;
-    
+
     // Creation of the user variable
     const user: UserForm = {
       id: null,
@@ -41,8 +43,16 @@ export class LoginComponent {
     };
 
     // Appel API
-    // 
-    console.log('account Logged');
-    console.log(user);
+    this.httpUserService.loginUser(user).subscribe({
+      next: (data) => {
+        console.log('account logged');
+        console.log(data);
+        this.router.navigate(['channels/1']);                
+      },
+      error: (err) => {
+        //console.error(err.error.error);
+        this.isError = true;
+      },
+    });
   }
 }
