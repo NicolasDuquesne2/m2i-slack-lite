18b01c43ccff11b56e5c94de082b44988477fa36
Loic2dot0 <moreaux.loic@gmail.com>
2023-09-01 11:49:35 +0200

    update signup form

--- front/src/app/component/signup/signup.component.ts
+++ front/src/app/component/signup/signup.component.ts
@@ -13,22 +13,22 @@ export class SignupComponent {
   isErrorName: boolean = false;
   isErrorPassword: boolean = false;
   isErrorPasswordConfirm: boolean = false;
-  
+
   isValidEmail: boolean = false;
   isValidName: boolean = false;
   isValidPassword: boolean = false;
-  isValidPasswordConfirm: boolean = false; 
+  isValidPasswordConfirm: boolean = false;
 
-  constructor(private formBuilder: FormBuilder){
+  constructor(private formBuilder: FormBuilder) {
     this.formSignup = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      passwordConfirm: ['', [Validators.required, Validators.minLength(8)]]
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^#_+=\-()[\]{}|\\:;"'<>,.?/~])[A-Za-z\d@$!%*?&^#_+=\-()[\]{}|\\:;"'<>,.?/~]{8,}$/)]],
+      passwordConfirm: ['', [Validators.required]]
     });
   }
 
-  onSignup(){
+  onSignup() {
     this.isError = false;
     this.isErrorEmail = false;
     this.isErrorName = false;
@@ -42,7 +42,16 @@ export class SignupComponent {
     this.formSignup.get('email')?.invalid ? this.isErrorEmail = true : this.isValidEmail = true;
     this.formSignup.get('name')?.invalid ? this.isErrorName = true : this.isValidName = true;
     this.formSignup.get('password')?.invalid ? this.isErrorPassword = true : this.isValidPassword = true;
-    this.formSignup.get('passwordConfirm')?.invalid ? this.isErrorPasswordConfirm = true : this.isValidPasswordConfirm;
+    
+    if(this.formSignup.get('passwordConfirm')?.invalid) {
+      this.isErrorPasswordConfirm = true;
+    } else{
+      if(this.formSignup.value.password !== this.formSignup.value.passwordConfirm){
+        this.isErrorPasswordConfirm = true;
+        return;
+      }
+      this.isValidPasswordConfirm = true;    
+    } 
     if (this.formSignup.invalid) return;
 
     // Appel API
