4b090e1a3923392c3e5f5f5af6e10e39fc82a2cc
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-09-01 11:23:35 +0200

    displays channels list

--- front/src/app/component/channel-list/channel-list.component.ts
+++ front/src/app/component/channel-list/channel-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { Channel } from 'src/app/interface/channel';
+import { HttpChannelService } from 'src/app/service/http-channel.service';
 
 @Component({
   selector: '[app-channel-list]',
@@ -6,4 +8,19 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./channel-list.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class ChannelListComponent {}
+export class ChannelListComponent {
+  channels: Channel[] = [];
+  localError!: Error;
+
+  constructor(private httpChannelService: HttpChannelService) {
+    this.httpChannelService.getChannels().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.channels = res;
+      },
+      error: (err) => {
+        console.error('something wrong occurred: ' + err.message);
+      },
+    });
+  }
+}
